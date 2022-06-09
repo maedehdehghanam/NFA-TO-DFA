ef23049c0248dfac94e59541d11c3305fa957391
Maedeh Dehghan <maedehdehghanam@gmail.com>
2022-06-10 00:52:06 +0430

    new sattes are added

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -22,6 +22,9 @@ for x in range(0,int(m)):
 		language_alphabets.append(alphabet);
 	ways.append(alphabet + " "+ s1+ " "+ s2);
 
+"""
+calculating epsilon closure
+"""
 def epsilon_closureـrecursive(state):
 	ec = set();
 	for x in range(0,len(ways)):
@@ -36,7 +39,37 @@ def epsilon_closure(state):
 	ec = (epsilon_closureـrecursive(state))
 	ec.add(state)
 	return ec;
-print(epsilon_closure('2'))
-'''
-ways[x].split()[0] == '-' and
-'''
+"""
+calculating delta
+"""
+def delta_calculator(state, alphabet):
+	delta = set()
+	for x in ways:
+		if x.split()[0] == alphabet and x.split()[1] == state:
+			delta.add(x.split()[2])
+	return delta;
+"""
+calculating table T
+"""
+def tabel_T(qi, alphabet):
+	ec = epsilon_closure(qi)
+	delta = set()
+	for q in ec:
+		delta = delta.union(delta_calculator(q,alphabet))
+	t = set()
+	for w in delta:
+		t= t.union(epsilon_closure(w))
+	return t;
+
+"""
+possible states
+"""
+new_states = list()
+for x in language_alphabets:
+	for y in range(1,int(n)+1):
+		bc = (tabel_T(str(y),x))
+		if bc not in new_states and bc !=set():
+			new_states.append(bc)
+print(new_states[1])
+
+
